91fe8358a011a7d5b65e55800809de36bdeb1e9f
bph <birgit.pauli@gmail.com>
2020-11-22 18:42:34 -0500

    First version

--- index.js
+++ index.js
@@ -10,7 +10,45 @@
 const chalk = require('chalk');
 const sym = require('log-symbols');
 
-module.exports = () => {
-    console.log(`this is working!`);
+const greenI = chalk.green.inverse;
+const green = chalk.green;
+const redI = chalk.red.bold.inverse;
+const red = chalk.red;
+const orange = chalk.keyword('orange');
+const orangeI = chalk.keyword('orange').inverse;
+const blue = chalk.blue;
+const blueI = chalk.blue.inverse;
+
+module.exports = options => {
+    const defaultOptions = {
+        type: `error`,
+        msg: `You forgot to define all options`,
+        name: ''
+
+    };
+    const opts = {...defaultOptions, ...options}; //Spread operator
+    const {type, msg, name} = opts; //Object destructuring
+    const printName = name ? name : type.toUpperCase()
+    // if a name is provided we use the name, if not we use the type and make it All Caps. 
+
+    if (type === `success`) {
+        console.log(`\n${sym.success} ${greenI(` ${printName} `)} ${green(msg)}\n`);
+    }
+
+    if (type === `error`) {
+        console.log(
+            `\n${sym.error} ${redI(` ${printName} `)} ${red(msg)}\n`);
+    }
+
+    if (type === `warning`) {
+        console.log(
+            `\n${sym.warning} ${orangeI(` ${printName} `)} ${orange(msg)}\n`);
+    }
+
+    if (type === `info`) {
+        console.log(
+            `\n${sym.info} ${blueI(` ${printName} `)} ${blue(msg)}\n`);
+    }
+
 }
 
--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 const alert = require('./index.js');
 
-alert({
-	type: "success",
-	msg: "All done!",
-});
+alert();
 
-// ' Success: All Done;
+alert({ type: "success", msg: "All done!", name: `Done`});
+
+alert({ type: "warning", msg: "Tomorrow it's going to be hot. Get up early!",});
+
+alert({ type: "info", msg: "No Hurricanes in the Gulf of Mexico",});
+
+alert({ type: "error", msg: "Oops that doesn't work. ",name: `Mistake!`});
